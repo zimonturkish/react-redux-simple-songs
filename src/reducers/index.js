c6015ff6a5237219de740d44cdc682135b3e8ae1
Simone Turco <simone.turco@charite.de>
2020-02-27 14:42:16 +0100

    correcting reducers typing mistake

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,12 +5,12 @@ const songsReducer = () => {
     { title: "Life on Mars", duration: "4:10" },
     { title: "Message in a bottle", duration: "4:51" },
     { title: "Kids", duration: "5:02" },
-    { title: "Sgt. Pepper's Lonely Hearts Club Band", duration: "1:59" }
+    { title: "Sgt. Pepper's Lonely Hearts Club Band", duration: "2:02" }
   ];
 };
 
 const selectedSongReducer = (selectedSong = null, action) => {
-  if (action.type === "SONG_SELCETED") {
+  if (action.type === "SONG_SELECTED") {
     return action.payload;
   }
   return selectedSong;
