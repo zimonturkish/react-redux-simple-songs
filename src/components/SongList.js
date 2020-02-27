84803e5d7407aad69f171f5582d8590efff77cb2
Simone Turco <simone.turco@charite.de>
2020-02-27 18:42:46 +0100

    defining mapStateToProps

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class SongList extends Component {
   render() {
@@ -6,4 +7,9 @@ class SongList extends Component {
   }
 }
 
-export default SongList;
+const mapStateToProps = state => {
+  console.log(state);
+  return state;
+};
+
+export default connect(mapStateToProps)(SongList);
