766be79819c282aac066993726225f3b9aa94705
Simone Turco <simone.turco@charite.de>
2020-02-27 18:56:29 +0100

    Jsx for buttons

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,9 +2,22 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class SongList extends Component {
+  renderList() {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui botton primary">Select</button>
+          </div>
+
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    console.log(this.props.songs);
-    return <div>SongList</div>;
+    return <div>{this.renderList()}</div>;
   }
 }
 
