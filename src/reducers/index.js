25d71a1b519bd7cfce9959769de7c15af67324e6
Simone Turco <simone.turco@charite.de>
2020-02-27 14:30:47 +0100

    adding 2 reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+import { act } from "react-dom/test-utils";
+
+const songsReducer = () => {
+  return [
+    { title: "Life on Mars", duration: "4:10" },
+    { title: "Message in a bottle", duration: "4:51" },
+    { title: "Kids", duration: "5:02" },
+    { title: "Sgt. Pepper's Lonely Hearts Club Band", duration: "1:59" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELCETED") {
+    return action.payload;
+  }
+};
