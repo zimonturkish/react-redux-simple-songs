3e66afae5b96110bc027595c12845b55534198ad
Simone Turco <simone.turco@charite.de>
2020-02-27 19:02:37 +0100

    correcting

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -7,7 +7,7 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui botton primary">Select</button>
+            <button className="ui button primary">Select</button>
           </div>
 
           <div className="content">{song.title}</div>
@@ -17,7 +17,7 @@ class SongList extends Component {
   }
 
   render() {
-    return <div>{this.renderList()}</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
