50ad0b9b87ff502c29af6d20d73f1b1ff46febd7
Simone Turco <simone.turco@charite.de>
2020-02-27 18:50:43 +0100

    console..

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,13 +3,13 @@ import { connect } from "react-redux";
 
 class SongList extends Component {
   render() {
+    console.log(this.props.songs);
     return <div>SongList</div>;
   }
 }
 
 const mapStateToProps = state => {
-  console.log(state);
-  return state;
+  return { songs: state.songs };
 };
 
 export default connect(mapStateToProps)(SongList);
