edb3c38014eb1d0d763a2ae2d72aafc8ade349d6
Turco <turcos@charite.de>
2020-02-28 12:47:20 +0100

    Updates

---
+++ precache-manifest.6e00b7f49bcf4401812fafbbf88d9d50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa54a7bbfcfcf1661bb054e72fe3efae",
+    "url": "/react-redux-simple-songs/index.html"
+  },
+  {
+    "revision": "a4e475b0f1d67ecdacb0",
+    "url": "/react-redux-simple-songs/static/js/2.98926fb9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-redux-simple-songs/static/js/2.98926fb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1b6dd3f53920ebf2bb",
+    "url": "/react-redux-simple-songs/static/js/main.b9b30e69.chunk.js"
+  },
+  {
+    "revision": "68f385dd6b7171fd75dd",
+    "url": "/react-redux-simple-songs/static/js/runtime-main.624868a8.js"
+  }
+]);
